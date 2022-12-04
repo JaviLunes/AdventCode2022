f554fc0394d7348add39463f53468e54792c5557
Javier <javilopez@uvigo.es>
2022-12-04 09:52:34 +0100

    Day 4 completed

---
+++ src/aoc2022/day_4/tools.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+"""Tools used for solving the Day 4: Camp Cleanup puzzle."""
+
+# Standard library imports:
+from collections.abc import Iterable
+
+
+class CleanupReviewer:
+    """Check for overlaps in section assignments between pairs of Elves."""
+    def __init__(self, assignment_pairs: list[str]):
+        self.pairs = list(self._process_assignments(pairs=assignment_pairs))
+
+    def _process_assignments(self, pairs: list[str]) -> Iterable[tuple[range, range]]:
+        """Transform each pair-of-section-assignments-string into two section ranges."""
+        for pair in pairs:
+            string_1, string_2 = pair.split(",")
+            sections_1 = self._assignment_to_range(string=string_1)
+            sections_2 = self._assignment_to_range(string=string_2)
+            yield sections_1, sections_2
+
+    @staticmethod
+    def _assignment_to_range(string: str) -> range:
+        """Build a range of section IDs from the string representation of such range."""
+        return (lambda a, b: range(int(a), int(b) + 1))(*string.split("-"))
+
+    @property
+    def count_ful_overlaps(self) -> int:
+        """Provide the count of pairs where one range fully contains the other."""
+        return sum([(set(a) <= set(b) or set(b) <= set(a)) for a, b in self.pairs])
+
+    @property
+    def count_partial_overlaps(self) -> int:
+        """Provide the count of pairs where one range partially contains the other."""
+        return sum([bool((set(a) & set(b))) for a, b in self.pairs])
---
+++ tests/tests_day_4.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+"""Tests for the Day 4: Camp Cleanup puzzle."""
+
+# Standard library imports:
+import unittest
+
+# Local application imports:
+from aoc2022.day_4.tools import CleanupReviewer
+
+
+class ExampleTests(unittest.TestCase):
+    def setUp(self) -> None:
+        """Define objects to be tested."""
+        sections = ["2-4,6-8", "2-3,4-5", "5-7,7-9", "2-8,3-7", "6-6,4-6", "2-6,4-8"]
+        self.reviewer = CleanupReviewer(assignment_pairs=sections)
+
+    def test_number_of_fully_overlapping_assignment_pairs(self):
+        """The number of assignment pairs with full overlap is 2."""
+        self.assertEqual(2, self.reviewer.count_ful_overlaps)
+
+    def test_number_of_partially_overlapping_assignment_pairs(self):
+        """The number of assignment pairs with partial overlap is 4."""
+        self.assertEqual(4, self.reviewer.count_partial_overlaps)
