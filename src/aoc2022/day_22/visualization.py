93b4be000689af9a698a11e917531cff060717e7
Javier <javilopez@uvigo.es>
2023-01-04 21:10:59 +0100

    Add traveller's position visualization for day 22

--- src/aoc2022/day_22/visualization.py
+++ src/aoc2022/day_22/visualization.py
@@ -6,28 +6,50 @@ from aoc_tools.visualizations.grid_blocks import CellND, Grid2DPlotter
 from matplotlib.figure import Figure
 
 # Local application imports:
-from aoc2022.day_22.tools import MonkeyBoard
+from aoc2022.day_22.tools import BoardTraveller, MonkeyBoard
 
 # Set constants:
-TILE_COLOURS = {"Off-map": (0, 30, 50), "Open": (255, 245, 245), "Wall": (150, 75, 40)}
+CELL_COLOURS = {"Open": (255, 245, 245), "Traveller": (255, 0, 0),
+                "Wall": (150, 75, 40), "Off-map": (0, 30, 50)}
 TILE_NAMES = {" ": "Off-map", ".": "Open", "#": "Wall"}
+
+CellMap = dict[tuple[int, int], CellND]
 
 
 def plot_board(board: MonkeyBoard) -> Figure:
     """Plot the tiles of a MonkeyBoard as 2D cells in a mosaic tessellation."""
-    # Create tessellation:
-    cells = list(_build_board_cells(board=board).values())
-    plotter = Grid2DPlotter(cells=cells, empty_value="Undefined", palette=TILE_COLOURS)
+    cells = _build_board_cells(board=board).values()
+    plotter = Grid2DPlotter(
+        cells=cells, empty_value="Undefined", palette=CELL_COLOURS,
+        legend=True, title=False)
     fig = plotter.plot_xy()
-    # Tweak figure:
-    fig.axes[0].title.set_visible(False)
     fig.axes[0].invert_yaxis()
     fig.tight_layout(pad=0.25)
     return fig
 
 
-def _build_board_cells(board: MonkeyBoard) -> dict[tuple[int, int], CellND]:
+def plot_traveller(traveller: BoardTraveller) -> Figure:
+    """Plot the current location of a BoardTraveller at its MonkeyBoard."""
+    cells = _build_board_cells(board=traveller.board)
+    cells.update(_build_traveller_cell(traveller=traveller))
+    plotter = Grid2DPlotter(
+        cells=cells.values(), palette=CELL_COLOURS, empty_value="Undefined",
+        legend=True, title=False, annotations_kwargs=dict(
+            size=24, color="white", weight="bold", ha="center", va="center"))
+    fig = plotter.plot_xy()
+    fig.axes[0].invert_yaxis()
+    fig.tight_layout(pad=0.25)
+    return fig
+
+
+def _build_board_cells(board: MonkeyBoard) -> CellMap:
     """Create one cell for each tile in the board, and map it to its row and column."""
     rows, columns = board.shape
     return {(row, col): CellND(x=col, y=row, value=TILE_NAMES[board.rows[row][col]])
             for col in range(columns) for row in range(rows)}
+
+
+def _build_traveller_cell(traveller: BoardTraveller) -> CellMap:
+    """Create a special cell holding the location of the traveller in the board."""
+    row, col, facing = traveller.coordinates
+    return {(row, col): CellND(x=col, y=row, value="Traveller", annotation=facing)}
--- tests/tests_day_22.py
+++ tests/tests_day_22.py
@@ -10,7 +10,7 @@ from matplotlib.figure import Figure
 
 # Local application imports:
 from aoc2022.day_22.tools import BoardTraveller
-from aoc2022.day_22.visualization import plot_board
+from aoc2022.day_22.visualization import plot_board, plot_traveller
 
 
 class ExampleTests(unittest.TestCase):
@@ -22,21 +22,37 @@ class ExampleTests(unittest.TestCase):
             "        ...#....", "        .....#..", "        .#......",
             "        ......#.", "", "10R5L5R10L4R5L5"]
         self.traveller = BoardTraveller.from_notes(monkey_notes=monkey_notes)
-        self.traveller.travel()
 
     def test_final_position(self):
         """Your row, column and facing after completing the travel are 6, 8, 0."""
+        self.traveller.travel()
         self.assertEqual(6, self.traveller.row + 1)
         self.assertEqual(8, self.traveller.column + 1)
         self.assertEqual("→", self.traveller.facing)
 
     def test_final_password(self):
         """The password revealed after completing the travel is 6032."""
+        self.traveller.travel()
         self.assertEqual(6032, self.traveller.pass_code)
 
     def test_plot_board(self):
         """Plot the tested MonkeyBoard."""
         fig = plot_board(board=self.traveller.board)
+        self.assertIsInstance(fig, Figure)
+        fig.show()
+        plt.close(fig)
+
+    def test_plot_start_traveller(self):
+        """Plot the tested BoardTraveller before the start of their walk."""
+        fig = plot_traveller(traveller=self.traveller)
+        self.assertIsInstance(fig, Figure)
+        fig.show()
+        plt.close(fig)
+
+    def test_plot_end_traveller(self):
+        """Plot the tested BoardTraveller at the end of their walk."""
+        self.traveller.travel()
+        fig = plot_traveller(traveller=self.traveller)
         self.assertIsInstance(fig, Figure)
         fig.show()
         plt.close(fig)
