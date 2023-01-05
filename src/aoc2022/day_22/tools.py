328a896efcce5bc7eee186d8669c7f5654de8ae2
Javier <javilopez@uvigo.es>
2023-01-05 12:36:48 +0100

    Make each day 22's board stripe walking return all positions walked over

--- src/aoc2022/day_22/tools.py
+++ src/aoc2022/day_22/tools.py
@@ -2,6 +2,7 @@
 """Tools used for solving the Day 22: Monkey Map puzzle."""
 
 # Standard library imports:
+from itertools import cycle, islice
 from math import inf
 
 # Set constants:
@@ -9,7 +10,8 @@ FACING_VALUES = {"→": 0, "↓": 1, "←": 2, "↑": 3}
 FACING_ARROWS = {v: k for k, v in FACING_VALUES.items()}
 
 
-Stripe = list[tuple[int, str]]
+Position = tuple[int, int, str]
+Stripe = list[tuple[Position, str]]
 
 
 class MonkeyBoard:
@@ -17,33 +19,29 @@ class MonkeyBoard:
     def __init__(self, board_rows: list[str]):
         self.rows = board_rows
 
-    def get_new_position(self, traveller: "BoardTraveller", steps: int) -> int:
-        """Location of a Traveller after walking in a straight line a number of steps."""
+    def get_walk_path(self, traveller: "BoardTraveller", steps: int) -> list[Position]:
+        """Traveller's row, column and facing after each step in a straight walk."""
         stripe = self._cut_walk_stripe(*traveller.coordinates)
         stripe = self._filter_off_tiles(stripe=stripe)
         indices, tiles = zip(*stripe)
         steps = min(steps, tiles.index("#") if "#" in tiles else inf)
-        return indices[(steps - 1) % len(indices)]
+        return list(islice(cycle(indices), steps)) if steps > 0 else indices[-1:]
 
     def _cut_walk_stripe(self, row: int, col: int, facing: str) -> Stripe:
         """All open, walled and off-map tiles ahead from the given position."""
-        if facing == "→":
+        assert facing in ["→", "←", "↓", "↑"], f"Invalid '{facing}' facing."
+        if facing in ["→", "←"]:
             tiles = self.rows[row]
-            stripe = [(i, tile) for i, tile in enumerate(tiles)]
-            return stripe[col + 1:] + stripe[:col + 1]
-        elif facing == "←":
-            tiles = self.rows[row]
-            stripe = [(i, tile) for i, tile in enumerate(tiles)]
-            return stripe[:col][::-1] + stripe[col:][::-1]
-        elif facing == "↓":
+            positions = [(row, c, facing) for c in range(len(tiles))]
+        else:
             tiles = "".join([row[col] for row in self.rows])
-            stripe = [(i, tile) for i, tile in enumerate(tiles)]
-            return stripe[row + 1:] + stripe[:row + 1]
-        elif facing == "↑":
-            tiles = "".join([row[col] for row in self.rows])
-            stripe = [(i, tile) for i, tile in enumerate(tiles)]
-            return stripe[:row][::-1] + stripe[row:][::-1]
-        raise ValueError
+            positions = [(r, col, facing) for r in range(len(tiles))]
+        start = positions.index((row, col, facing))
+        stripe = list(zip(positions, tiles))
+        if facing in ["→", "↓"]:
+            return stripe[start + 1:] + stripe[:start + 1]
+        else:
+            return stripe[:start][::-1] + stripe[start:][::-1]
 
     @staticmethod
     def _filter_off_tiles(stripe: Stripe) -> Stripe:
@@ -90,10 +88,11 @@ class BoardTraveller:
 
     def _move(self, n_tiles: int):
         """Move a number of tiles (or until hitting a wall) over a Stripe."""
+        positions = self.board.get_walk_path(traveller=self, steps=n_tiles)
         if self.facing in ["→", "←"]:
-            self.column = self.board.get_new_position(traveller=self, steps=n_tiles)
+            self.column = positions[-1][1]
         else:
-            self.row = self.board.get_new_position(traveller=self, steps=n_tiles)
+            self.row = positions[-1][0]
 
     def _rotate(self, direction: str):
         """Rotate the current facing 90° clockwise (R) or anti-clockwise (L)."""
@@ -102,7 +101,7 @@ class BoardTraveller:
         self.facing = FACING_ARROWS[new_facing_value]
 
     @property
-    def coordinates(self) -> tuple[int, int, str]:
+    def coordinates(self) -> Position:
         """Provide a tuple with the current row, column and facing of this Traveller."""
         return self.row, self.column, self.facing
 
